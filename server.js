146ba689e7af17e48bee3370fbde2773ee0caf58
gregoryjohncarter <gregoryjohncarter@gmail.com>
2022-03-05 13:42:12 -0500

    server.js synchronous issue

--- server.js
+++ server.js
@@ -1,23 +1,20 @@
 const path = require('path');
 const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
 const routes = require('./controllers');
-const sequelize = require('./config/connection');
-const helpers = require('./utils/helpers');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.urlencoded({ extended: true }));
-
-const session = require('express-session');
-
+const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {},
+  cookie: {
+    expires: 3600000
+  },
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -27,14 +24,19 @@ const sess = {
 
 app.use(session(sess));
 
-// turn on routes
-app.use(routes);
+const helpers = require('./utils/helpers');
 
-const exphbs = require('express-handlebars');
 const hbs = exphbs.create({ helpers });
 
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+// turn on routes
+app.use(routes);
 
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
